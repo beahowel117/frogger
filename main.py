941af3e56d76b410d9124b3951a1f519d0a6a18f
Beth <beahowel117@gmail.com>
2023-02-05 17:19:19 -0500

    created turtle and made move across screen

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from player import Player
 from car_manager import CarManager
@@ -9,8 +9,16 @@ screen.setup(width=600, height=600)
 screen.title("Frogger")
 screen.tracer(0)
 
+frog = Player()
+
+screen.listen()
+screen.onkeypress(frog.cross_street, "Up")
+
 game_is_on = True
 while game_is_on:
+    #tracer is off, instead game (everything in loop) will be refreshed every .1s
     time.sleep(0.1)
     screen.update()
+
+screen.exitonclick()
 
